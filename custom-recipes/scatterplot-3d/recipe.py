30cddd41f004846e0fe2c1e43a1f652922b3491e
admin <zztanaka@gmail.com>
2022-07-26 17:50:10 +0900

    Edited files

--- custom-recipes/scatterplot-3d/recipe.py
+++ custom-recipes/scatterplot-3d/recipe.py
@@ -1,60 +1,9 @@
-# Code for custom code recipe scatterplot-3d (imported from a Python recipe)
-
-# To finish creating your custom recipe from your original PySpark recipe, you need to:
-#  - Declare the input and output roles in recipe.json
-#  - Replace the dataset names by roles access in your code
-#  - Declare, if any, the params of your custom recipe in recipe.json
-#  - Replace the hardcoded params values by acccess to the configuration map
-
-# See sample code below for how to do that.
-# The code of your original recipe is included afterwards for convenience.
-# Please also see the "recipe.json" file for more information.
-
-# import the classes for accessing DSS objects from the recipe
 import dataiku
-# Import the helpers for custom recipes
 from dataiku.customrecipe import *
-
-# Inputs and outputs are defined by roles. In the recipe's I/O tab, the user can associate one
-# or more dataset to each input and output role.
-# Roles need to be defined in recipe.json, in the inputRoles and outputRoles fields.
-
-# To  retrieve the datasets of an input role named 'input_A' as an array of dataset names:
-input_A_names = get_input_names_for_role('input_A_role')
-# The dataset objects themselves can then be created like this:
-input_A_datasets = [dataiku.Dataset(name) for name in input_A_names]
-
-# For outputs, the process is the same:
-output_A_names = get_output_names_for_role('main_output')
-output_A_datasets = [dataiku.Dataset(name) for name in output_A_names]
-
-
-# The configuration consists of the parameters set up by the user in the recipe Settings tab.
-
-# Parameters must be added to the recipe.json file so that DSS can prompt the user for values in
-# the Settings tab of the recipe. The field "params" holds a list of all the params for wich the
-# user will be prompted for values.
-
-# The configuration is simply a map of parameters, and retrieving the value of one of them is simply:
-my_variable = get_recipe_config()['parameter_name']
-
-# For optional parameters, you should provide a default value in case the parameter is not present:
-my_variable = get_recipe_config().get('parameter_name', None)
-
-# Note about typing:
-# The configuration of the recipe is passed through a JSON object
-# As such, INT parameters of the recipe are received in the get_recipe_config() dict as a Python float.
-# If you absolutely require a Python int, use int(get_recipe_config()["my_int_param"])
-
-
-#############################
-# Your original recipe
-#############################
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-import dataiku
 import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
+
+from pandas.api.types import is_string_dtype
+from pandas.api.types import is_numeric_dtype
 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib
@@ -63,38 +12,73 @@ import matplotlib.pyplot as plt
 import os
 import io
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Read recipe inputs
-ecommerce_transactions_with_ip_prepared = dataiku.Dataset("ecommerce_transactions_with_ip_prepared")
-df = ecommerce_transactions_with_ip_prepared.get_dataframe()
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Filter to only US records
-df_usa = df[df["MerchantIP_country"] == "United States"]
+### READ PLUGIN INPUTS ###
 
-# Determine average purchase amount, per customer age and purchase hour
-df_avg_purchase = df[["PurchaseHour", "CustomerAge", "OrderTotal"]].groupby(by=["PurchaseHour",
-                                                                                "CustomerAge"], as_index=False).mean()
+# Retrieve input and output dataset names
+input_dataset_name = get_input_names_for_role('input_dataset')[0]
+output_folder_name = get_output_names_for_role('main_output')[0]
 
-# Scatter plot of avg purchase amount, per customer age and purchase hour
+# Retrieve mandatory user-defined parameters
+plot_title = get_recipe_config().get('plot_title', "Scatter Plot Title")
+    
+x_axis = get_recipe_config().get('x_axis')
+y_axis = get_recipe_config().get('y_axis')
+z_axis = get_recipe_config().get('z_axis')
+
+# Retrieve optional user-defined parameters
+filter_column = get_recipe_config().get('filter_column')
+filter_value = get_recipe_config().get('filter_value')
+
+# Read input dataset as dataframe
+input_dataset = dataiku.Dataset(input_dataset_name)
+df = input_dataset.get_dataframe()
+
+
+### ERROR CHECKING OF USER INPUTS ###
+
+# Check that x, y and z axis correspond to column names
+if (x_axis not in df.columns) or (y_axis not in df.columns) or (z_axis not in df.columns):
+    raise KeyError("X-axis, Y-axis, and Z-axis parameters must be columns in the input dataset.")
+    
+# Check that x, y, and z axis columns contain numeric values
+if (not is_numeric_dtype(df[x_axis])) or (not is_numeric_dtype(df[y_axis])) or (not is_numeric_dtype(df[z_axis])):
+    raise ValueError("X-axis, Y-axis, and Z-axis columns should only contain numeric values.")
+
+# Check that the filter column is part of the dataframe (if defined)
+if (filter_column != '') and (filter_column not in df.columns):
+    raise KeyError("If defined, the filter column parameter must be a column in the input dataset.")
+    
+    
+### GENERATE 3D SCATTER PLOT ###
+
+# Filter values in dataset (if filter_column is defined)
+if filter_column != '':
+    df = df[df[filter_column].astype(str) == str(filter_value)] # force a string comparison
+
+# Determine average z-axis value, for overlapping x-axis and y-axis values
+df_avg_purchase = df[[x_axis, y_axis, z_axis]].groupby(by=[x_axis, y_axis], as_index=False).mean()
+
+# Construct scatter plot 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-xs = df_avg_purchase["PurchaseHour"]
-ys = df_avg_purchase["CustomerAge"]
-zs = df_avg_purchase["OrderTotal"]
+xs = df_avg_purchase[x_axis]
+ys = df_avg_purchase[y_axis]
+zs = df_avg_purchase[z_axis]
 
-ax.set_xlabel('Purchase Hour')
-ax.set_ylabel('Customer Age')
-ax.set_zlabel('Avg Order Total')
-ax.set_title("US Transactions Only")
+ax.set_xlabel(x_axis)
+ax.set_ylabel(y_axis)
+ax.set_zlabel(z_axis)
+ax.set_title(plot_title)
 
 ax.scatter(xs, ys, zs)
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Save scatter plot to folder
-folder_for_plots = dataiku.Folder("MIrujfBg")
+
+### SAVE SCATTER PLOT TO FOLDER ###
+
+folder_for_plots = dataiku.Folder(output_folder_name)
 
 buffer = io.BytesIO()
 plt.savefig(buffer, format="png")
-folder_for_plots.upload_stream("US Transactions Only.png", buffer.getvalue())
+folder_for_plots.upload_stream(plot_title + ".png", buffer.getvalue())
